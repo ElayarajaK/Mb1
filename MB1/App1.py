3e9ea049ee2d8b52a077c24135853640fd1675fb
Elayaraja <kelaya5@gmail.com>
2023-10-12 11:16:14 +0530

    Add files via upload

---
+++ MB1/App1.py
@@ -0,0 +1,32 @@
+Name="git JavaScript react"
+print(Name[6])
+#print(Name[20])
+print(len(Name))
+lastIndex=len(Name)-1
+print("lastIndex is : ",lastIndex," Value : ",Name[lastIndex])
+Mid=int(len(Name)/2)
+
+print("Mid index is : ",Mid," Value is : ",Name[Mid])
+
+n=10
+l=20
+n=n+l  #n=30
+l=n-l #l=10
+n=n-l #n=20
+name1="java"
+index=0
+for temp in name1:
+	print("index is : ",index , " value is ",temp)
+	index+=1;
+
+
+
+
+
+
+
+'''
+temp=n #temp =10 n=10 l=20
+n=l;  #n=20 l=20 temp=10
+l=temp # n=20 l=10
+'''
